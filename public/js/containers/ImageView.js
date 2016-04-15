5544309ab886be9b8861cb9748295add5cb5040d
GeordieP <geordie.powers@gmail.com>
2016-04-14 20:35:57 -0400

    Changed navigation in ResultsView and ImageView as some characters were not universally supported

--- public/js/containers/ImageView.js
+++ public/js/containers/ImageView.js
@@ -12,7 +12,7 @@ let ImageViewShape = React.createClass({
 		return (
 			<div className="viewArea">
 				<div className="viewArea--controls">
-					<Link to="/"><p>◄ Back to results</p></Link><a href="#" target="_blank"> | Original Image</a>
+					<Link to="/"><p>&lt;-- Back to results</p></Link><a href="#" target="_blank"> | Original Image</a>
 				</div>
 				<div className="viewArea--images">
 					<img src={this.ImgUrlFromID(this.props.params.imageID)}/>
