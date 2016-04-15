b80386e313db91585c8875350f80c74a442882f8
GeordieP <geordie.powers@gmail.com>
2016-04-15 01:45:38 -0400

    Searches can now be sent using the enter/return key

--- public/js/components/SearchBox.js
+++ public/js/components/SearchBox.js
@@ -2,13 +2,20 @@ import React, { PropTypes } from 'react';
 
 const SearchBox = ({ onUpdateSearchTerm }) =>  {
 	let inputElement;
+	let submit = () => {
+		onUpdateSearchTerm(inputElement.value);
+		inputElement.value = 'enter a search term...';
+	}
 	return (
 		<div className="searchBox">
-			<input ref={node => { inputElement = node }} type="text" defaultValue="enter a search term..." onClick={ () => inputElement.value = '' }></input>
-			<button onClick={ () => {
-				onUpdateSearchTerm(inputElement.value);
-				inputElement.value = 'enter a search term...';
-			}} >
+			<input 
+				ref={node => inputElement = node}
+				type="text" 
+				defaultValue="enter a search term..."
+				onClick={() => inputElement.value = '' }
+				onKeyPress={(e) => { if (e.charCode === 13) submit() }}>
+			</input>
+			<button onClick={() => submit() } >
 			Search
 			</button>
 		</div>
